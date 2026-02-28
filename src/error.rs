778c152cbe088ce731a6b6b56deca231c04ee88f
ishrut <iamishrut@gmail.com>
2026-02-28 17:06:43 +0000

    added errors

--- src/error.rs
+++ src/error.rs
@@ -1,12 +1,21 @@
 
 #[derive(Debug)]
 pub enum Error {
+    WaylandConnectError(wayland_client::ConnectError),
+    WaylandDispatchError(wayland_client::DispatchError),
+    WaylandSurfaceConfigurationError,
+    IoError(std::io::Error),
 }
 
 impl std::error::Error for Error {}
 
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "")
+        match &self {
+            Self::WaylandConnectError(e) => write!(f, "{}", e),
+            Self::WaylandDispatchError(e) => write!(f, "{}", e),
+            Self::WaylandSurfaceConfigurationError => write!(f, "wayland surface not configured"),
+            Self::IoError(e) => write!(f, "{}", e)
+        }
     }
 }
